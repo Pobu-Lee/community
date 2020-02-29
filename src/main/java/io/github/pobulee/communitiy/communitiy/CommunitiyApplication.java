af18fadb36e2805689b19246b0ab0f1c724dbdc6
Pobu-Lee <jiawei@bupt.edu.cn>
2020-02-29 13:15:55 +0800

    first commit

---
+++ src/main/java/io/github/pobulee/communitiy/communitiy/CommunitiyApplication.java
@@ -0,0 +1,13 @@
+package io.github.pobulee.communitiy.communitiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitiyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitiyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/pobulee/communitiy/communitiy/CommunitiyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pobulee.communitiy.communitiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitiyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
